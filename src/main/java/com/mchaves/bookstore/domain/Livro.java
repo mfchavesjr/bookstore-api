37f4a2ff9fa76ddda066ec3b8de5141e476fefe5
mfchavesjr <feira.nova@gmail.com>
2021-10-14 09:48:36 -0300

    Criação das classes de dominio Categoria e Livro

---
+++ src/main/java/com/mchaves/bookstore/domain/Livro.java
@@ -0,0 +1,76 @@
+package com.mchaves.bookstore.domain;
+
+import java.util.Objects;
+
+public class Livro {
+    private Integer id;
+    private String titulo;
+    private String autor;
+    private String texto;
+    private Categoria categoria;
+
+    public Livro() {
+        super();
+    }
+
+    public Livro(Integer id, String titulo, String autor, String texto, Categoria categoria) {
+        this.id = id;
+        this.titulo = titulo;
+        this.autor = autor;
+        this.texto = texto;
+        this.categoria = categoria;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getTexto() {
+        return texto;
+    }
+
+    public void setTexto(String texto) {
+        this.texto = texto;
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Livro)) return false;
+        Livro livro = (Livro) o;
+        return Objects.equals(getId(), livro.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+}
